d3454c8378f66167cf58d786b083eb63b663a524
Chris Shiplet <chris@nearengine.com>
2019-09-21 16:51:48 -0700

    Add the codes

--- README.md
+++ README.md
@@ -1,2 +1,70 @@
-# babybump
-A tiny command line utility for managing your project's version numbers
+# fistbump
+
+[![Crates.io](https://img.shields.io/crates/v/fistbump.svg)](https://crates.io/crates/fistbump)
+[![license](http://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/chrisshiplet/fistbump/blob/master/LICENSE)
+
+A tiny command line utility for managing your project's version numbers.
+
+## Installation
+
+Obtain the binary from the Releases page and put it somewhere on your PATH. Make sure to `chmod +x` the binary on Linux and MacOS.
+
+_With Cargo:_ Run `cargo install fistbump`
+
+## Usage
+
+### Configuration
+
+`fistbump` is used with a configuration file that lives in your project directory. To specify a configuration, create a file in your project's root directory called `.fistbumprc.json`.
+
+#### Example
+
+```json
+{
+  "current_version": "0.1.0",
+  "files": [
+    {
+      "path": "Cargo.toml",
+      "search": "version = \"{current_version}\"",
+      "replace": "version = \"{new_version}\""
+    },
+    {
+      "path": "Cargo.lock",
+      "search": "name = \"fistbump\nversion = \"{current_version}\"",
+      "replace": "name = \"fistbump\nversion = \"{new_version}\""
+    }
+  ],
+  "search": "{current_version}",
+  "replace": "{new_version}"
+}
+```
+
+#### Defaults
+
+The `search` and `replace` config is always optional. They default to `{current_version}` and `{new_version}`, respectively. The only required fields are `current_version` and `files` (which must each contain a `path`).
+
+### Arguments
+
+When you create a new release on your project, run `fistbump <version>` where `<version>` is the new version string. For example: `fistbump 1.0.0`.
+
+### Environment Variables
+
+Set `FISTBUMP_LOG_LEVEL` to view detailed logs while `fistbump` is running. The default value is `error`. Valid values are `debug`, `info`, `error`. Example to view all output: `FISTBUMP_LOG_LEVEL=debug fistbump 1.0.0`.
+
+## Develop
+
+Run `FISTBUMP_LOG_LEVEL=debug cargo run -- 1.0.0` to view all debugging output.
+
+## Publish
+
+- `fistbump <version>`
+- `cargo update`
+- `git commit`, `git tag`, `git push --all`
+- `cargo publish --dry-run`
+- `cargo publish`
+
+## Contribute
+
+If `fistbump` makes your life easier, consider contributing to further development by donating to [bitcoin:18Xg5pLi4ueMCpoNHt5of44AirPhzjXXQ2](bitcoin:18Xg5pLi4ueMCpoNHt5of44AirPhzjXXQ2)
+
+If you would like to contribute code to the project, feel free to submit pull requests for existing issues or file an issue for an enhancement or bug request. Pull requests will be merged at the sole discretion of the maintainer.
---
+++ src/main.rs
@@ -0,0 +1,119 @@
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+
+use env_logger::Env;
+use regex::Regex;
+use serde::{Deserialize, Serialize};
+use std::fs;
+use structopt::StructOpt;
+
+#[derive(Serialize, Deserialize)]
+struct File {
+    path: String,
+    search: Option<String>,
+    replace: Option<String>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Config {
+    current_version: String,
+    search: Option<String>,
+    replace: Option<String>,
+    files: Vec<File>,
+}
+
+#[derive(StructOpt)]
+struct CliArgs {
+    version: String,
+}
+
+const CONFIG_PATH: &str = ".fistbumprc.json";
+
+fn fistbump() -> Result<(), String> {
+    info!("Initialized");
+
+    let args = CliArgs::from_args();
+
+    info!("Reading config from file {}", CONFIG_PATH);
+    let config_data = match fs::read_to_string(CONFIG_PATH) {
+        Ok(n) => n,
+        Err(_) => return Err(format!("Couldn't read config file {}", CONFIG_PATH)),
+    };
+    debug!("Got config `{}`", config_data);
+
+    info!("Parsing config from JSON");
+    let config: Config = match serde_json::from_str(&config_data) {
+        Ok(n) => n,
+        Err(e) => {
+            return Err(format!(
+                "Invalid JSON in config file {}: {}",
+                CONFIG_PATH, e
+            ))
+        }
+    };
+
+    let current_version = config.current_version;
+    let new_version = &args.version;
+    let default_search = config.search.unwrap_or("{current_version}".to_string());
+    let default_replace = config.replace.unwrap_or("{new_version}".to_string());
+
+    for file in config.files.iter() {
+        info!("Processing file {}", &file.path);
+        let file_search = file
+            .search
+            .as_ref()
+            .unwrap_or(&default_search)
+            .replace("{current_version}", &current_version);
+        debug!("Searching for `{}`", file_search);
+
+        let file_replace = file
+            .replace
+            .as_ref()
+            .unwrap_or(&default_replace)
+            .replace("{new_version}", new_version);
+        debug!("Replacing with `{}`", file_replace);
+
+        let file_contents = match fs::read_to_string(&file.path) {
+            Ok(n) => n,
+            Err(e) => return Err(format!("Couldn't open file {}: {}", &file.path, e)),
+        };
+
+        let re = match Regex::new(&file_search) {
+            Ok(n) => n,
+            Err(e) => return Err(format!("Regex error: {}", e)),
+        };
+
+        let updated_file_contents = re.replace_all(&file_contents, file_replace.as_str());
+
+        match fs::write(&file.path, updated_file_contents.as_ref()) {
+            Ok(n) => n,
+            Err(e) => return Err(format!("Couldn't write file {}: {}", &file.path, e)),
+        };
+    }
+
+    let updated_config_data = config_data.replace(
+        format!(r#""current_version": "{}""#, &current_version).as_str(),
+        format!(r#""current_version": "{}""#, &new_version).as_str(),
+    );
+    debug!("Updating config `{}`", updated_config_data);
+
+    match fs::write(CONFIG_PATH, updated_config_data) {
+        Ok(n) => n,
+        Err(e) => return Err(format!("Couldn't write config file: {}", e)),
+    };
+
+    info!("Finished");
+    return Ok(());
+}
+
+fn main() {
+    let env = Env::default().filter_or("FISTBUMP_LOG_LEVEL", "error");
+
+    env_logger::init_from_env(env);
+
+    if let Err(e) = fistbump() {
+        error!("{}", e);
+        ::std::process::exit(1);
+    }
+}
